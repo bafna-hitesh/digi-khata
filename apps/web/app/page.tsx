43092c74458f318b2e86893561ec11c9b1ed09c2
rachit1994 <rachit.srivastava1994@gmail.com>
2023-07-16 11:59:07 +0530

    antd setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,8 @@
 // app/page.tsx
-import { Provider, Button } from '@digi/components'
+import { Button } from '@digi/components'
 
 export default function Page() {
   return (
-    <Provider>
-      <Button>Click me</Button>
-    </Provider>
+    <Button>Click me</Button>
   )
 }
---
+++ packages/with-antd/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Button } from 'antd';
+
+const DigiButton = () => (
+  <Button type='primary'>hello</Button>
+);
+
+export default DigiButton;
