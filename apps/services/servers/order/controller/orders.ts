b730de11b60498131dcd1da4373670e082964f4e
Manas Singh <manassingh557@gmail.com>
2023-08-27 19:43:42 +0530

    Upload data to Kafka

--- apps/services/servers/order/controller/orders.ts
+++ apps/services/servers/order/controller/orders.ts
@@ -1,3 +1,47 @@
-export const ordersUpload = async (req, res) => {
+import { NextFunction, Request, Response } from "express";
+import busboy from "busboy";
+import csv from "csv-parser";
+import { createKafkaInstance, kafkaProducer } from "@digi/kafka";
+import config from "../config";
+
+export const ordersUpload = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // Connecting to Kafka
+    let kafka = createKafkaInstance(config.KAFKA_HOST.split(','));
+    let producer = kafkaProducer.createKafkaProducer(kafka);
+    await kafkaProducer.checkProducerConnectionToKafka(producer);
+
+    // Parse the file uploaded and upload the data to Kafka
+    const bb = busboy({ headers: req.headers });
+
+    bb.on('file', (name, file, info) => {
+
+        file
+        .pipe(csv())
+        .on('data', (row) => {
+          row.broker = 'Kite';
+          // Todo - Get the user from req object. This user will be set when we verify the request that contains token in the middleware
+          row.user = 'Some Kite User';
+          kafkaProducer.produceDataToKafka(producer, 'orders', '0', row);
+        })
+        .on('end', () => {
+          console.log('CSV processing finished');
+        })
+        .on('error', (error) => {
+          throw error;
+        })
+    });
+
+    bb.on('close', () => {
+      res.send('Parsed CSV Successfully');
+    });
   
+    req.pipe(bb);
+
+  } catch (error: any){
+    next({
+      status: 500,
+      message: error.message
+    })
+  }
 }
---
+++ packages/kafka/index.ts
@@ -0,0 +1,15 @@
+import { Kafka } from "kafkajs";
+import * as kafkaProducer from "./kafkaProducer";
+import * as kafkaConsumer from "./kafkaConsumer";
+
+function createKafkaInstance(brokersURL: any) {
+  return new Kafka({
+    brokers: brokersURL
+  });
+}
+
+export {
+  createKafkaInstance,
+  kafkaProducer,
+  kafkaConsumer
+}
