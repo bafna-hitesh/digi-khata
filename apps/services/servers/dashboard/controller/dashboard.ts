fd9e946b0965850627c05948d49e18c249eec045
Manas Singh <manassingh557@gmail.com>
2023-09-17 12:08:46 +0530

    Rebasing to master

--- apps/services/servers/dashboard/controller/dashboard.ts
+++ apps/services/servers/dashboard/controller/dashboard.ts
@@ -4,17 +4,14 @@ import config from '../config';
 
 export const getDashboardData = async (req: Request, res: Response, next: NextFunction) => {
   // try {
-  let body = req.body;
-  let data: any = await druid.getKiteDailyFOData(
-    config.DRUID_ROUTER_URL,
-    'Some Kite User',
-    'Kite',
-    'FO',
-    body.freq,
-    '2023-07-06',
-    '2023-07-06',
-  );
-  return res.json(data);
+    let body = req.body;
+    let calendarData: any = await druid.getKiteDailyFOData(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
+    let dataByDayOfWeek: any = await druid.getKiteFODataByDayOfWeek(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
+    
+    return res.json({
+      "calendar": calendarData,
+      "performanceByDayOfTheWeek": dataByDayOfWeek
+    });
   // } catch(err) {
   // console.log('Some Exception Occurred');
   // console.error(err);
--- packages/druid/daily.ts
+++ packages/druid/daily.ts
@@ -1,31 +1,59 @@
-import runQuery from './runQuery';
-import * as query from './query';
-import { DateTime } from 'luxon';
+import runQuery from "./runQuery";
+import * as query from "./query";
 
 async function getKiteFOBuyForTheDay(currentDate: any, routerURL: string, user: string, broker: string, type: string) {
-  currentDate = DateTime.fromISO(currentDate);
   let parameters = [
-    { type: 'TIMESTAMP', value: currentDate },
-    { type: 'TIMESTAMP', value: currentDate.plus({ days: 1 }).toISODate() }, // Incrementing Date since Druid doesn't include end Date
-    { type: 'VARCHAR', value: user },
-    { type: 'VARCHAR', value: broker },
-    { type: 'VARCHAR', value: type },
-  ];
+    { "type": "TIMESTAMP", "value": currentDate },
+    { "type": "TIMESTAMP", "value": currentDate.add(1, 'day').format('YYYY-MM-DD') }, // Incrementing Date since Druid doesn't include end Date
+    { "type": "VARCHAR", "value": user },
+    { "type": "VARCHAR", "value": broker },
+    { "type": "VARCHAR", "value": type },
+  ]
   let kiteFOBuyData = await runQuery(routerURL, query.getKiteFOBuyForTheDay, parameters);
   return kiteFOBuyData.data;
 }
 
 async function getKiteFOSellForTheDay(currentDate: any, routerURL: string, user: string, broker: string, type: string) {
-  currentDate = DateTime.fromISO(currentDate);
   let parameters = [
-    { type: 'TIMESTAMP', value: currentDate },
-    { type: 'TIMESTAMP', value: currentDate.plus({ days: 1 }).toISODate() }, // Incrementing Date since Druid doesn't include end Date
-    { type: 'VARCHAR', value: user },
-    { type: 'VARCHAR', value: broker },
-    { type: 'VARCHAR', value: type },
-  ];
+    { "type": "TIMESTAMP", "value": currentDate },
+    { "type": "TIMESTAMP", "value": currentDate.add(1, 'day').format('YYYY-MM-DD') }, // Incrementing Date since Druid doesn't include end Date
+    { "type": "VARCHAR", "value": user },
+    { "type": "VARCHAR", "value": broker },
+    { "type": "VARCHAR", "value": type },
+  ]
   let kiteFOSellData = await runQuery(routerURL, query.getKiteFOSellForTheDay, parameters);
   return kiteFOSellData.data;
 }
 
-export { getKiteFOBuyForTheDay, getKiteFOSellForTheDay };
+async function getKiteFOBuyForWeekDay(weekDay: any, startDate: any, endDate: any, routerURL: string, user: string, broker: string, type: string) {
+  let parameters = [
+    { "type": "TIMESTAMP", "value": startDate },
+    { "type": "TIMESTAMP", "value": endDate.add(1, 'day').format('YYYY-MM-DD') }, // Incrementing Date since Druid doesn't include end Date
+    { "type": "VARCHAR", "value": user },
+    { "type": "VARCHAR", "value": broker },
+    { "type": "VARCHAR", "value": type },
+    { "type": "INTEGER", "value": weekDay }
+  ]
+  let kiteFOBuyData = await runQuery(routerURL, query.getKiteFOBuyForDayOfTheWeek, parameters);
+  return kiteFOBuyData.data;
+}
+
+async function getKiteFOSellForWeekDay(weekDay: any, startDate: any, endDate: any, routerURL: string, user: string, broker: string, type: string) {
+  let parameters = [
+    { "type": "TIMESTAMP", "value": startDate },
+    { "type": "TIMESTAMP", "value": endDate.add(1, 'day').format('YYYY-MM-DD') }, // Incrementing Date since Druid doesn't include end Date
+    { "type": "VARCHAR", "value": user },
+    { "type": "VARCHAR", "value": broker },
+    { "type": "VARCHAR", "value": type },
+    { "type": "INTEGER", "value": weekDay }
+  ]
+  let kiteFOBuyData = await runQuery(routerURL, query.getKiteFOSellForDayOfTheWeek, parameters);
+  return kiteFOBuyData.data;
+}
+
+export {
+  getKiteFOBuyForTheDay,
+  getKiteFOSellForTheDay,
+  getKiteFOBuyForWeekDay,
+  getKiteFOSellForWeekDay
+}
