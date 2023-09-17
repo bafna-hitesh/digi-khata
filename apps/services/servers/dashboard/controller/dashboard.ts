84d0fb539a36c70eb65935db8a76e9a13eea7761
Manas Singh <manassingh557@gmail.com>
2023-09-17 12:13:40 +0530

    Rebasing to master

--- apps/services/servers/dashboard/controller/dashboard.ts
+++ apps/services/servers/dashboard/controller/dashboard.ts
@@ -4,13 +4,16 @@ import config from '../config';
 
 export const getDashboardData = async (req: Request, res: Response, next: NextFunction) => {
   // try {
+    // Todo - Validate Input
     let body = req.body;
-    let calendarData: any = await druid.getKiteDailyFOData(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
+    let calendarData: any = await druid.getKiteFODataDaily(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
     let dataByDayOfWeek: any = await druid.getKiteFODataByDayOfWeek(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
+    let hourlyData: any = await druid.getKiteFODataByHourly(config.DRUID_ROUTER_URL, 'Some Kite User', 'Kite', 'FO', body.freq, '2023-07-06', '2023-07-07');
     
     return res.json({
       "calendar": calendarData,
-      "performanceByDayOfTheWeek": dataByDayOfWeek
+      "performanceByDayOfTheWeek": dataByDayOfWeek,
+      "timeAnalysis": hourlyData
     });
   // } catch(err) {
   // console.log('Some Exception Occurred');
