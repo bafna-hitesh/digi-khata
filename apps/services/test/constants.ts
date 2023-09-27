cdf1c5947c7ca8d31c76599cabefb3161f896ff2
Manas Singh <manassingh557@gmail.com>
2023-09-27 23:21:40 +0530

    Changes for PR comments

---
+++ apps/services/test/constants.ts
@@ -0,0 +1,9 @@
+export default {
+  'routerURL' : 'http://localhost:8888',
+  'user' : 'Some Kite User',
+  'broker' : 'Kite',
+  'commodity' : 'FO',
+  'freq' : 'Daily',
+  'startDate' : '2023-07-06',
+  'endDate' : '2023-07-07'
+}
