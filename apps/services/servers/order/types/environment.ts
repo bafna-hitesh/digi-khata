961087d8de9bba7e0a44d32d1b732c4671f4ebfc
Manas Singh <manassingh557@gmail.com>
2023-12-25 23:15:07 +0530

    feat: Weekly postgres backup on Friday 22:00

--- apps/services/servers/order/types/environment.ts
+++ apps/services/servers/order/types/environment.ts
@@ -23,4 +23,9 @@ export interface ProcessEnv {
 
   REDIS_HOST: string;
   REDIS_PORT: number;
+
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_DEFAULT_REGION: string;
+  AWS_BACKUP_BUCKET: string;
 }
