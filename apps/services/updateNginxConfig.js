6db112781429d310c67915f6d26b19d7c7a425a2
rachit1994 <rachit.srivastava1994@gmail.com>
2023-12-29 21:35:34 +0530

    nginx update

--- apps/services/updateNginxConfig.js
+++ apps/services/updateNginxConfig.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable consistent-return */
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
 const fs = require('fs').promises;
 const { exec } = require('child_process');
 const os = require('os');
@@ -13,34 +14,17 @@ const sslCertPath = path.join(__dirname, 'ssl', 'nginx.crt');
 const sslKeyPath = path.join(__dirname, 'ssl', 'nginx.key');
 
 const generateSSLCertificates = async () => {
-  // Ensure the ssl directory exists
   const sslDir = path.join(__dirname, 'ssl');
-  await fs.mkdir(sslDir, { recursive: true }).catch((err) => {
+  try {
+    await fs.mkdir(sslDir, { recursive: true });
+  } catch (err) {
     throw new Error(`Failed to create SSL directory: ${err.message}`);
-  });
+  }
 
   return new Promise((resolve, reject) => {
     const command = `openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${sslKeyPath} -out ${sslCertPath} -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=localhost"`;
-
-    exec(command, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
+    exec(command, (error) => (error ? reject(error) : resolve()));
   });
-};
-
-const checkAndGenerateSSLCertificates = async () => {
-  try {
-    await fs.access(sslCertPath);
-    await fs.access(sslKeyPath);
-    console.log('SSL certificates already exist.');
-  } catch {
-    console.log('Generating SSL certificates...');
-    await generateSSLCertificates();
-  }
 };
 
 const installNginx = () => {
@@ -57,20 +41,16 @@ const installNginx = () => {
       return;
     }
 
-    exec(command, (error) => {
-      if (error) {
-        return reject(error);
-      }
-      resolve();
-    });
+    exec(command, (error) => (error ? reject(error) : resolve()));
   });
 };
 
-const findNginxConfigPath = async () => {
+const findNginxConfigPath = () => {
   return new Promise((resolve, reject) => {
     exec('nginx -t 2>&1', { encoding: 'utf8' }, (error, stdout) => {
       if (error) {
-        return reject(error);
+        reject(error);
+        return;
       }
       const match = stdout.match(/nginx: the configuration file (.+?) syntax is ok/);
       resolve(match ? match[1] : null);
@@ -79,51 +59,52 @@ const findNginxConfigPath = async () => {
 };
 
 const run = async () => {
-  let nginxConfigPath;
-
   try {
-    nginxConfigPath = await findNginxConfigPath();
-  } catch (error) {
-    console.log('Nginx not found, installing...');
-    await installNginx();
-    nginxConfigPath = await findNginxConfigPath();
-  }
+    console.log('Generating SSL certificates...');
+    await generateSSLCertificates();
 
-  if (!nginxConfigPath) {
-    console.error('Could not find or install Nginx.');
-    process.exit(1);
-  }
+    let nginxConfigPath = await findNginxConfigPath();
 
-  const localNginxConfigPath = path.join(__dirname, 'nginx.conf');
-  const newConfigData = await fs.readFile(localNginxConfigPath, 'utf8');
-
-  const userPort = process.env.USER_MS_PORT || '6000';
-  const orderPort = process.env.ORDER_MS_PORT || '7000';
-  const dashboardPort = process.env.DASHBOARD_MS_PORT || '8000';
-  const validatePort = process.env.VALIDATE_MS_PORT || '8000';
-
-  await checkAndGenerateSSLCertificates();
-
-  const updatedNewConfig = newConfigData
-    .replace('USER_MS_PORT', userPort)
-    .replace('ORDER_MS_PORT', orderPort)
-    .replace('DASHBOARD_MS_PORT', dashboardPort)
-    .replace('VALIDATE_MS_PORT', validatePort)
-    .replace('/path/to/your/certificate.pem', sslCertPath)
-    .replace('/path/to/your/private.key', sslKeyPath);
-
-  // Replace the existing nginx.conf with the new configuration
-  await fs.writeFile(nginxConfigPath, updatedNewConfig, 'utf8');
-
-  exec('nginx -s reload', (error) => {
-    if (error) {
-      console.error(`Error reloading Nginx: ${error}`);
-      return;
+    if (!nginxConfigPath) {
+      console.log('Nginx not found, installing...');
+      await installNginx();
+      nginxConfigPath = await findNginxConfigPath();
     }
-    console.log('Nginx configuration updated and reloaded.');
-  });
+
+    if (!nginxConfigPath) {
+      console.error('Could not find or install Nginx.');
+      process.exit(1);
+    }
+
+    const localNginxConfigPath = path.join(__dirname, 'nginx.conf');
+    const newConfigData = await fs.readFile(localNginxConfigPath, 'utf8');
+
+    const replacements = {
+      USER_MS_PORT: process.env.USER_MS_PORT || '6000',
+      ORDER_MS_PORT: process.env.ORDER_MS_PORT || '7000',
+      DASHBOARD_MS_PORT: process.env.DASHBOARD_MS_PORT || '8000',
+      VALIDATE_MS_PORT: process.env.VALIDATE_MS_PORT || '8000',
+      '/path/to/your/certificate.pem': sslCertPath,
+      '/path/to/your/private.key': sslKeyPath,
+    };
+
+    const updatedNewConfig = Object.entries(replacements).reduce(
+      (config, [key, value]) => config.replace(new RegExp(key, 'g'), value),
+      newConfigData,
+    );
+
+    await fs.writeFile(nginxConfigPath, updatedNewConfig, 'utf8');
+
+    exec('nginx -s reload', (error) => {
+      if (error) {
+        console.error(`Error reloading Nginx: ${error}`);
+        return;
+      }
+      console.log('Nginx configuration updated and reloaded.');
+    });
+  } catch (error) {
+    console.error(`An error occurred: ${error}`);
+  }
 };
 
-run().catch((error) => {
-  console.error(`An error occurred: ${error}`);
-});
+run();
