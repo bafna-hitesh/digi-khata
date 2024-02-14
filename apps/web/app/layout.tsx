ece3f25ccddde4b189c432ce742170119917f9e6
bafna-hitesh <hitesh.bafna00@gmail.com>
2024-02-14 22:17:31 +0530

    rtk setup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,6 +6,7 @@ import darkTheme from 'config/darkTheme';
 import { Inter } from 'next/font/google';
 import './flex.scss';
 import './global.scss';
+import StoreProvider from './StoreProvider';
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
@@ -19,7 +20,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={inter.className}>
         <AntdRegistry>
           <ConfigProvider theme={darkTheme}>
-            <App className='full-width full-height'>{children}</App>
+            <StoreProvider>
+              <App className='full-width full-height'>{children}</App>
+            </StoreProvider>
           </ConfigProvider>
         </AntdRegistry>
       </body>
